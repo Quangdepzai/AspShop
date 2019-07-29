384ed74785d25c21a6e4fc8cd2db89d8f132732a
Quangkuns <quangtv.firecoals@gmail.com>
2019-07-29 21:13:12 +0700

    Insert User

---
+++ Shop/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using Models.Dao;
+using Models.EF;
+using OnlineShop.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.Areas.Admin.Controllers
+{
+    public class UserController : BaseController
+    {
+        // GET: Admin/User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Edit(int id)
+        {
+            var user = new UserDao().ViewDetail(id);
+            return View(user);
+        }
+        public ActionResult Create(User user)
+        {
+            var dao = new UserDao();
+            var Pass = Encryptor.MD5Hash(user.Password);
+            // user.Password = Pass;
+            user.PasswordLevel2 = Pass;
+            long id = dao.Insert(user);
+            if (ModelState.IsValid)
+            {
+                if (id > 0)
+                {
+                    return RedirectToAction("Index", "User");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Thêm User thành công");
+                }
+            }
+            else
+            {
+
+            }
+            return View("Index");
+        }
+    }
+}
